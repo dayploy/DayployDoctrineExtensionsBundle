88b23a27eb4833ac20177750b53aff2b1e5a0877
Thomas Beaujean <thomas.beaujean@dayploy.fr>
2025-10-06 08:51:57 +0200

    init

---
+++ src/DayployDoctrineExtensionsBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dayploy\DoctrineExtensionsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class DayployDoctrineExtensionsBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @return void
+     */
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
